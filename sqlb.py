55426b8537f39d379e24800076aa7b8b8b1a15b9
ClarkMitchell <ccm26@students.uwf.edu>
2015-02-18 10:34:48 -0600

    created db, testing gitignore

--- sqlb.py
+++ sqlb.py
@@ -1,8 +1,8 @@
 import sqlite3
-with sqlite2.connect("new.db") as connection:
+with sqlite3.connect("new.db") as connection:
     c = connection.cursor()
 
-c.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
-c.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
+    c.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+    c.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
 
-conn.close()
+    c.close()
